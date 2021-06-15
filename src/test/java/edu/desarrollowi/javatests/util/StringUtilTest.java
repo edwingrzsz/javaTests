c92fa3702ac458b73e29cd646469478e41471b87
edwingrzsz <edwing9@gmail.com>
2021-06-15 15:13:04 -0500

    Aniadiremos excepciones

--- src/test/java/edu/desarrollowi/javatests/util/StringUtilTest.java
+++ src/test/java/edu/desarrollowi/javatests/util/StringUtilTest.java
@@ -3,6 +3,17 @@ package edu.desarrollowi.javatests.util;
 public class StringUtilTest {
     public static void main(String[] args) {
         String result = StringUtil.repeat("hola",3);
-        System.out.println(result);
+
+        if(!result.equals("holaholahola")){
+            System.out.println("ERROR");
+        }
+
+        String result2 = StringUtil.repeat("hola",1);
+
+        if(!result2.equals("hola")){
+            System.out.println("ERROR");
+        }
+
+
     }
 }
