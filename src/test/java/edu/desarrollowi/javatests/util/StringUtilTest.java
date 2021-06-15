eb2a3975e2bc1dd781c75f96c2b693a9a214c737
edwingrzsz <edwing9@gmail.com>
2021-06-15 15:09:39 -0500

    Prueba unitaria clase StringUtil

---
+++ src/test/java/edu/desarrollowi/javatests/util/StringUtilTest.java
@@ -0,0 +1,8 @@
+package edu.desarrollowi.javatests.util;
+
+public class StringUtilTest {
+    public static void main(String[] args) {
+        String result = StringUtil.repeat("hola",3);
+        System.out.println(result);
+    }
+}
